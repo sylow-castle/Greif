bffbfd76ce3342524733e377e5f2807324162baa
weakhold <weakhold@mail.fake>
2015-09-16 02:02:41 +0900

    出力するdotファイルの文字コードをUTF-8を使うように変更

--- src/runner/TextFileWriter.java
+++ src/runner/TextFileWriter.java
@@ -1,9 +1,12 @@
 package runner;
 
 import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 public class TextFileWriter {
@@ -14,8 +17,9 @@ public class TextFileWriter {
   }
   public void writeFile(List<String> content){
       try{
-        FileWriter out = new FileWriter(this.FullPath);
-        BufferedWriter wr  = new BufferedWriter(out);
+        OutputStream fos = new FileOutputStream(this.FullPath);
+        OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+        BufferedWriter wr  = new BufferedWriter(osw);
 
         //テキストファイルの内容をリストとして展開
         for (String Line : content){
