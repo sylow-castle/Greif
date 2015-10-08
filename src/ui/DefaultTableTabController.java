62794b10d4bb31a4d082033b84328b9921fcb860
weakhold <weakhold@mail.fake>
2015-10-09 00:42:36 +0900

    ・DefaultTable.fxmlのTableViewの変数名を変更 ・チケット#4（新規レコードが修正されないバグ）への対応。

--- src/ui/DefaultTableTabController.java
+++ src/ui/DefaultTableTabController.java
@@ -1,23 +1,17 @@
 package ui;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
 import table.Column;
 import table.SimpleStringTable;
 import ui.DefaultRow;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.collections.SetChangeListener;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Button;
@@ -25,6 +19,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
@@ -38,32 +34,21 @@ public class DefaultTableTabController implements Initializable {
   @FXML
   public Button del;
 
-  public ObservableList<DefaultRow> list;
-
   @FXML
-  public TableView<DefaultRow> table;
+  public TableView<DefaultRow> tableView;
 
-  private SimpleStringTable modelTable;
-
-  public DefaultRow defaultValue;
-  public Map<DefaultRow, Map<Column, String>> index;
-
-
+  private ObjectProperty<SimpleStringTable> modelTable = new SimpleObjectProperty<>();
+  private final DefaultRow defaultValue = new DefaultRow();
+  private final Map<DefaultRow, Map<Column, String>> index = new HashMap<>();;
 
   public void initialize(URL url, ResourceBundle rb) {
-    //デフォルト値の設定
-    //tableのTableColumnのリストを受けて変化する。
-    defaultValue = new DefaultRow();
-    index = new HashMap<>();
-
     //リストの設定
-    this.list = FXCollections.observableArrayList();
-    table.setEditable(true);
-    table.setItems(list);
+    tableView.setEditable(true);
+    tableView.setItems(FXCollections.observableArrayList());
 
     TableColumn<DefaultRow, String> col = new TableColumn<DefaultRow, String>();
     col.setText("編集");
-    table.getColumns().add(col);
+    tableView.getColumns().add(col);
 
     col.setCellFactory(new Callback<TableColumn<DefaultRow, String>, TableCell<DefaultRow, String>>() {
       @Override
@@ -71,52 +56,11 @@ public class DefaultTableTabController implements Initializable {
         return new ButtonTableCell<DefaultRow, String>();
       }
     });
-
-
-    //Columnの追加・削除時の動作、
-    ListChangeListener<TableColumn<DefaultRow, ?>> listener = new ListChangeListener<TableColumn<DefaultRow, ?>>() {
-      @Override
-      public void onChanged(ListChangeListener.Change<? extends TableColumn<DefaultRow, ?>> e) {
-        for (TableColumn<DefaultRow, ?> column : e.getList()) {
-          String columnName = column.getText();
-          defaultValue.getValueMap().put(columnName, new SimpleStringProperty(columnName));
-
-        }
-      }
-    };
-    table.getColumns().addListener(listener);
-
   }
 
-  public void addColumn(Column column) {
-    String columnName = column.getName();
-    TableColumn<DefaultRow, String> addColumn = new TableColumn<DefaultRow, String>();
-    addColumn.setEditable(true);
-    addColumn.textProperty().bind(column.nameProperty());
-    addColumn.setId(columnName);
-    addColumn.setCellFactory(TextFieldTableCell.<DefaultRow> forTableColumn());
-    addColumn.setCellValueFactory(
-        new Callback<CellDataFeatures<DefaultRow, String>, ObservableValue<String>>() {
-          @Override
-          public ObservableValue<String> call(CellDataFeatures<DefaultRow, String> p) {
-            return p.getValue().getValueMap().get(columnName);
-          }
-        });
-
-    addColumn.setOnEditCommit(event -> {
-      //TODO Table側リスナーからの通知で値を変更するようにしたい
-      index.get(event.getRowValue()).put(column, event.getNewValue());
-      event.getRowValue().getValueMap().get(columnName).set(event.getNewValue());;
-
-      event.getRowValue().getValueMap().keySet().stream()
-      .forEach(key -> System.out.println(event.getRowValue().getValueMap().get(key).get()));
-    });
-
-    table.getColumns().add(addColumn);
-  }
 
   public void setTableModel(SimpleStringTable model) {
-    this.modelTable = model;
+    this.modelTable.setValue(model);
 
     //ボタンの設定
     add.setOnAction((ActionEvent e) -> {
@@ -129,7 +73,7 @@ public class DefaultTableTabController implements Initializable {
     });
 
     del.setOnAction((ActionEvent e) -> {
-      DefaultRow row = table.getSelectionModel().getSelectedItem();
+      DefaultRow row = tableView.getSelectionModel().getSelectedItem();
       model.removeRecord(this.index.get(row));
     });
 
@@ -137,21 +81,53 @@ public class DefaultTableTabController implements Initializable {
       @Override
       public void onChanged(SetChangeListener.Change<? extends Column> change) {
         if (change.wasAdded()) {
-          String columnName = change.getElementAdded().getName();
-          addColumn(change.getElementAdded());
-          for (DefaultRow row : table.getItems()) {
-            row.getValueMap().put(columnName, new SimpleStringProperty("new value"));
+          Column column = change.getElementAdded();
+          String columnName = column.getName();
+
+          //defaultValueの整備
+          StringProperty columnNameProp = new SimpleStringProperty(columnName);
+          columnNameProp.bind(column.nameProperty());
+          defaultValue.getValueMap().put(columnName, columnNameProp);
+
+          //tableViewに追加するTableColumnの設定
+          TableColumn<DefaultRow, String> addColumn = new TableColumn<DefaultRow, String>();
+          addColumn.setEditable(true);
+          addColumn.textProperty().bind(column.nameProperty());
+          addColumn.setId(columnName);
+          addColumn.setCellFactory(TextFieldTableCell.<DefaultRow> forTableColumn());
+          addColumn.setCellValueFactory(
+              new Callback<CellDataFeatures<DefaultRow, String>, ObservableValue<String>>() {
+                @Override
+                public ObservableValue<String> call(CellDataFeatures<DefaultRow, String> p) {
+                  return p.getValue().getValueMap().get(columnName);
+                }
+              });
+
+          addColumn.setOnEditCommit(event -> {
+            //modelTableの操作。
+            //TODO modelTable側に設定したリスナーからの通知で値を変更するようにしたい
+              index.get(event.getRowValue()).put(column, event.getNewValue());
+              event.getRowValue().getValueMap().get(columnName).set(event.getNewValue());
+            });
+          tableView.getColumns().add(addColumn);
+
+          //既存のレコードへの値の追加
+          for (DefaultRow row : tableView.getItems()) {
+            row.getValueMap().put(columnName, new SimpleStringProperty(columnName));
           }
+
         }
 
         if (change.wasRemoved()) {
           String columnName = change.getElementRemoved().getName();
-          //編集ボタンのカラムのために(notNullの条件がついてる
-          table.getColumns().removeIf(column -> (null != column.getId() && column.getId().equals(columnName)));
+          //編集ボタンのカラムのために(notNullの条件がついてる）
+          tableView.getColumns().removeIf(column -> (null != column.getId() && column.getId().equals(columnName)));
 
-          for (DefaultRow row : table.getItems()) {
+          for (DefaultRow row : tableView.getItems()) {
             row.getValueMap().remove(columnName);
           }
+
+          defaultValue.getValueMap().remove(columnName);
         }
       }
     });
@@ -174,19 +150,18 @@ public class DefaultTableTabController implements Initializable {
               row.setValueMap(FXCollections.observableMap(rowData));
 
               //追加操作
-              table.getItems().add(row);
+              tableView.getItems().add(row);
 
-              //
               index.put(row, record);
             }
           }
 
           if(change.wasRemoved()) {
             for(Map<Column, String> record : change.getRemoved()) {
-              table.getItems().stream()
+              tableView.getItems().stream()
                 .filter(row -> index.get(row).equals(record))
                 .findFirst()
-                .ifPresent(row -> list.remove(row));
+                .ifPresent(row -> tableView.getItems().remove(row));
             }
           }
         }
